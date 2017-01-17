ad1835d6e9260795003af72d4c472ad39382a532
Lukasz <l.bielenin@gmail.com>
2017-01-17 13:35:27 +0100

    login alpha

---
+++ login.py
@@ -0,0 +1,36 @@
+from open_lists import *
+
+
+class Start:
+    def login_check(self):
+
+
+
+        valid = ""
+        valid_pass = ""
+
+        while valid == "":
+            login = input("Login: ")
+            password = input("Password: ")
+
+            for student in Open().open_users("students.csv"):
+                if student[2] == login:
+                    valid = student[2]
+                    valid_pass = student[3]
+                    if password == valid_pass:
+                        print("student")
+            for mentor in Open().open_users("Mentors.csv"):
+                if mentor[2] == login:
+                    valid = mentor[2]
+                    valid_pass = mentor[3]
+                    if password == valid_pass:
+                        print("mentor")
+            for employee in Open().open_users("employees.csv"):
+                if employee[2] == login:
+                    valid = employee[2]
+                    valid_pass = employee[3]
+                    if password == valid_pass:
+                        print("employee")
+            print("Your login or password is incorrect.")
+
+Start().login_check()
