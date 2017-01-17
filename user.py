91ff10619d3f2ec52ca982f5b32b5486cacfacb7
ztokarski <zbigniew.piotr.tokarski@gmail.com>
2017-01-17 23:30:29 +0100

    updated user.py: +docstrings +change_state +making lol from string +making loo from lol. Good Job.

--- user.py
+++ user.py
@@ -1,103 +1,122 @@
 class User():
+    '''
+    Class for user.
+    '''
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.login = name.lower()+surname.lower()
         self.password = name.lower()+"123"
+        self.state = True
 
     def __str__(self):
         return "{} {} {} {}".format(self.name, self.surname, self.login, self.password)
+
+    def change_state(self):
+        '''
+        change user's status (Activ/Disactiv)
+        '''
+        self.state = not self.state
+
+    def display_state(self):
+        if self.state == True:
+            return("Activ")
+        else:
+            return("Disactiv")
 
 
 class Student(User):
+    '''
+    Class for student.
+    '''
     def __init__(self, name, surname):
         User.__init__(self, name, surname)
-        # self.points = []
 
     def __str__(self):
-        return "{} {} {} {}".format(self.name, self.surname, self.login, self.password)
-
-
-
-
-    def add_points(self, points):
-            self.points.append(points)
-
-    def remove_points(self, points):
-        try:
-            self.points.remove(points)
-        except ValueError as point_error:
-            print(point_error)
+        return "{} {} {} {} {}\n".format(self.name, self.surname, self.login, self.password, self.state)
 
 
 
 class StudentList():
+    '''
+    Class for list of students.
+    '''
     def __init__(self):
         self.students_list = []
 
     def __str__(self):
         all_students = ""
         for item in self.students_list:
-            all_students += str(item)
+            if item.state == True: # only Activ Students
+                all_students += str(item)
         return all_students
+
+    def display_sl(self, string):
+        '''
+        Making list of lists (lol) from string
+        '''
+        self.string = str(string)
+        lol = [x.split(" ") for x in self.string.split("\n")]
+        return lol[:-1]
+
+    def display_ol(self, lol):
+        '''
+        Making list of objects from list of lists
+        '''
+        self.lol = lol
+        self.loo = []
+        for item in self.lol:
+            obj = Student(item[0], item[1])
+            obj.login = item[2]
+            obj.password = item[3]
+            obj.state = item[4]
+            self.loo.append(obj)
+        return self.loo
+
 
     def add_student(self, student):
         if isinstance (student, Student):
             self.students_list.append(student)
 
 
-
-
 class Employee(User):
+    '''
+    Class for Employee.
+    '''
     def __init__(self, name, surname):
         super().__init__(name, surname)
 
     def __str__(self):
-        return "{} {} {} {}".format(self.name, self.surname, self.password)
+        return "{} {} {} {} {}\n".format(self.name, self.surname, self.login, self.password, self.state)
 
 
-            # def __str__(self):
-            #     all_subject = " "
-            #     for item in self.subject:
-            #         all_subject += str(item)
-            #     return Person.__str__(self) + " I teach {}.".format(all_subject)
+class Manager(Employee):
+    '''
+    Class for Manager.
+    '''
+    def __init__(self, name, surname):
+        Employee.__init__(self, name, surname)
+
+    def __str__(self):
+        return "{} {} {} {} {}\n".format(self.name, self.surname, self.login, self.password)
 
 
-def convert_str_to_list(string):
-    return list(string.split(" "))
+'''TEST TEST TEST'''
 
 user1 = Student("Jan", "Kowalski")
-user2 = Student("Tomasz", "Nowak" )
+user2 = Student("Tomasz", "Nowak")
+user3 = Student("Adolf", "Dupa")
 
 students_list = StudentList()
+
 students_list.add_student(user1)
 students_list.add_student(user2)
+students_list.add_student(user3)
 print(students_list)
 
-# lista = convert_str_to_list(str(students_list))
-# print(lista)
-# a=["Adam","Kuś"]
-#
-# def Student_from_list(lista):
-#     b=Student(lista[0], lista[1])
-#     return b
-#
-# a=["Adam","Kuś"]
-# print(Student_from_list(lista))
+lol = (students_list.display_sl(students_list))
+print(lol)
 
-
-# user1.add_points(10); user1.add_points(20); user1.add_points(31)
-# print(user1)
-# print(user2)
-#
-# user2 = Student("Adam", "Nowak")
-# user2.add_points(12); user2.add_points(24); user2.add_points(30)
-# # print(user2)
-#
-# students_list = StudentList()
-# students_list.add_student(user1)
-# students_list.add_student(user2)
-# students_list.add_student("koko")
-#
-# print(students_list)
-
+loo = (students_list.display_ol(lol))
+print(loo)
+# print(loo[0].name)
