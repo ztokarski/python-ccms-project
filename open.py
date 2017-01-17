3c414d2e9b98ca36b6b232e90bd2afbfd5f30f9a
Lukasz <l.bielenin@gmail.com>
2017-01-17 11:35:22 +0100

    same

--- open.py
+++ open.py
@@ -16,3 +16,12 @@ class Open:
         mentors_list.append(mentor.strip().split("\t"))
         read_file.close()
         return mentors_list
+
+    @classmethod
+    def open_mentors(self):
+        employee_list = []
+        read_file = open("employees.csv", "r")
+        for employee in read_file:
+        employee_list.append(employee.strip().split("\t"))
+        read_file.close()
+        return employee_list
