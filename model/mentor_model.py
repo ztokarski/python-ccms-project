7e24f50bcf388af52e1fce6a9a88505d7f77d37c
Tomek Bujakowski <bujakowski@gmail.com>
2017-02-15 14:29:08 +0100

    Add model of getting and adding students and mentors from db

---
+++ model/mentor_model.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+class Mentor_model:
+
+    def __init__(self):
+        self.conn = sqlite3.connect('ccms.db')
+        self.db = self.conn.cursor()
+
+    def get_mentors_list(self):
+        mentors_list = self.db.execute("SELECT * FROM users where ID_role = 2")
+        return mentors_list
+
+    def add_mentor(self, *args):
+        self.conn.execute("INSERT INTO `users`(`name`,`surname`,`login`, `ID_role`) VALUES ('{}','{}','{}', 2);".format(*args))
+        self.conn.commit()
+
+
+if __name__ == '__main__':
+    model = Mentor_model()
+    for i in model.get_mentors_list():
+        print(i)
+
+    model.add_mentor('Rubu', 'Bubu', 'rbb')
+
+    for i in model.get_mentors_list():
+        print(i)
--- model/student_model.py
+++ model/student_model.py
@@ -3,8 +3,24 @@ import sqlite3
 
 class Student_model:
     
-    conn = sqlite3.connect('ccms.db')
+    def __init__(self):
+        self.conn = sqlite3.connect('ccms.db')
+        self.db = self.conn.cursor()
 
-    students_db = conn.execute("SELECT * FROM users WHERE id_role = 1")
+    def get_students_list(self):
+        students_list = self.db.execute("SELECT * FROM users where ID_role = 1")
+        return students_list
 
-    # return students_db
+    def add_student(self, *args):
+        self.conn.execute("INSERT INTO `users`(`name`,`surname`,`login`) VALUES ('{}','{}','{}');".format(*args))
+        self.conn.commit()
+
+if __name__ == '__main__':
+    model = Student_model()
+    for i in model.get_students_list():
+        print(i)
+
+    model.add_student('Stude', "Studencik" ,'ss')
+
+    for i in model.get_students_list():
+        print(i)
