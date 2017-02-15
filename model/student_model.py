adf9df7c20e1c76fa1f67eb73562fef1c74c3416
Lukasz <l.bielenin@gmail.com>
2017-02-15 10:47:51 +0100

    students db remodelled

---
+++ model/student_model.py
@@ -0,0 +1,10 @@
+import sqlite3
+import control\students
+
+class Student_model:
+    
+    conn = sqlite3.connect('ccms.db')
+
+    students_db = conn.execute("SELECT * FROM users WHERE id_role = 1")
+
+    return students_db
