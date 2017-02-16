c42801956fb338f9c0d3c0e7492576ab8b04a0bc
Tomek Bujakowski <bujakowski@gmail.com>
2017-02-16 18:44:41 +0100

    Revert to 0e4195a45b92f

--- model/user_model.py
+++ model/user_model.py
@@ -2,13 +2,5 @@ import sqlite3
 class User_model:
     conn = sqlite3.connect('ccms.db')
     users = conn.execute("SELECT * FROM users")
-
-    @classmethod
-    def get_student_and_mentors(cls):
-        s_and_m_list = []
-        s_and_m = cls.users.execute('SELECT * FROM users WHERE ID_role = 1 OR ID_role = 2')
-
-        for user in s_and_m:
-            s_and_m_list.append([user[0], user[1], user[2], user[3], user[7]])
-
-        return s_and_m_list
+    for i in users:
+        print(i)
