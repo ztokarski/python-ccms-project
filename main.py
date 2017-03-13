759766bb34c06043da27bb5a69b7e654d11e6c5d
Łukasz <l.bielenin@gmail.com>
2017-03-13 14:59:11 +0100

    connection to database SEEMS to work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from model.student_model import *
 from flask import Flask, render_template, request
 from configure import DATABASE as db
+import sqlite3
 
 app = Flask(__name__)
 app.database = db
@@ -14,6 +15,19 @@ def index():
 @app.route("/main.html")
 def main():
     return render_template("main.html")
+
+@app.route("/test")
+def test():
+    c = sqlite3.connect(app.database)
+    conn = c.cursor()
+    db_list = conn.execute("SELECT * FROM users;")
+    my_list = []
+    for row in db_list:
+
+        my_list.append(row)
+
+    return render_template("test_list.html", my_list=my_list)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
